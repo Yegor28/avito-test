23fc84aa3b7707a9caa66a3007376ab1b7455891
yegorp <yeagorpereverzev@gmail.com>
2020-05-30 22:40:49 +0300

    add "getAd" handler

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 	_ "github.com/lib/pq"
 	"io/ioutil"
 	"net/http"
+	"strings"
+
+	//"strings"
+
+	//"strconv"
 	"time"
 	"fmt"
 	"github.com/go-playground/validator"
@@ -23,7 +28,7 @@ const (
 )
 
 type ad struct {
-	Id int  `json: "ID"`
+	id int  `json: "ID"`
 	Name        string `json:"Name" validate:"required,lte=200"`
 	Description string `json:"Description" validate:"required,lte=1000"`
 	Photos      []string `json:"Photos" validate:"required,lte=3"`
@@ -108,18 +113,77 @@ func createAd(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	for row.Next() {
-		row.Scan(&advert.Id)
+		row.Scan(&advert.id)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, `{"result":"success", "id": %d}`, advert.Id)
+	fmt.Fprintf(w, `{"result":"success", "id": %d}`, advert.id)
+}
+func getAd(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	db, err := dbConnect(host, port, user, password, dbname)
+	if err != nil {
+		var info errInfo
+		fmt.Printf("Can't connect to database. Error in function dbConnect. Param's function host: %s," +
+			" user: %s, port: %s, password: s, dbname: %s \n", host, user, port, password, dbname)
+		info.Err = err
+		info.Info = "Can't connect to database. Invalid data."
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+	defer db.Close()
+
+	statement := "SELECT * FROM advert where id=$1;"
+	stmt, err := db.Prepare(statement)
+	if err != nil {
+		var info errInfo
+		fmt.Printf("Can't prepare query. Error in db.Prepare function. Param's function %s.\n", statement)
+		info.Err = err
+		info.Info = "Can't prepare query"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+	var advert ad
+	stmt.QueryRow(vars["id"]).Scan(&advert.id,&advert.Name, &advert.Description, pq.Array(&advert.Photos), &advert.Price)
+
+	fields:=r.URL.Query().Get("fields")
+
+	switch fields {
+	case "description":
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w,`{"Name": "%s", "Description":"%s", "Price": %d, "Photo": "%s"}`,
+			advert.Name, advert.Description, advert.Price, advert.Photos[0])
+		return
+	case "photos":
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w,`{"Name": "%s", "Price": %d, "Photos": [%+q]}`,
+			advert.Name, advert.Price, strings.Join(advert.Photos, ", "))
+		return
+	case "all":
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(advert)
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"Name": "%s", "Price": %d, "Photo": "%s"}`, advert.Name, advert.Price, advert.Photos[0])
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+}
+
+func getAdsList(w http.ResponseWriter, r *http.Request)  {
+	
 }
 
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/ads", createAd).Methods("POST")
-
+	r.HandleFunc("/ads/{id:[0-9]+}", getAd).Methods("GET")
 	server := http.Server{
 		Addr: ":228",
 		Handler: r,
